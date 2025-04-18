ed633f90dcc9a75b0aa1355e64d516c1642bb275
damiankryzia71 <damiankryzia71@gmail.com>
2025-04-18 16:51:54 -0700

    setup

--- backend/bingus/bingus/settings.py
+++ backend/bingus/bingus/settings.py
@@ -87,7 +87,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": "bingus",
         "USER": "postgres",
-        "PASSWORD": "udemy",
+        "PASSWORD": "3686",
         "HOST": "127.0.0.1",
         "PORT": "5432",
     }
